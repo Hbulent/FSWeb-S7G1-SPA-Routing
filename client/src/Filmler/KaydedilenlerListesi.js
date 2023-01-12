52a2ab894b60c9c297572c32271c013b11711fa6
Hbulent <bulent5354@gmail.com>
2023-01-12 13:07:28 +0300

    proje tamamlandı

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import { useHistory, Link } from "react-router-dom";
 
 export default function KaydedilenlerListesi(props) {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push("/");
+  };
+
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
-      {props.list.map(movie => (
+      {props.list.map((movie) => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Anasayfa</div>
+      <div onClick={handleClick} className="home-button">
+        Anasayfa
+      </div>
     </div>
   );
 }
